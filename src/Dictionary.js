cf226e111cff18979bd81ce8d20f2266543c9527
Anna Podobrii <anna@nearmeentertainment.com>
2022-10-14 20:03:13 +0300

    added pictures

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result"
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [result, setResult] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleRespons(response) {
     setResult(response.data[0])
@@ -15,10 +17,20 @@ export default function Dictionary() {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleRespons);
+
+     let pexelsApiKey =
+       "563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
+     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function setKeywordFunc(event) {
     setKeyword(event.target.value);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   return (
@@ -26,6 +38,7 @@ export default function Dictionary() {
       <form className="text-center" onSubmit={search}>
         <input type="search" onChange={setKeywordFunc} />
         <Result result={result} />
+        <Photos photos={photos} />
       </form>
     </div>
   );
